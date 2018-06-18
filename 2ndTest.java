e7b4c528db67fe65e4782671c20d8a19b3a15910
Sampaath <31530528+Sampaath@users.noreply.github.com>
2018-06-18 15:06:33 -0500

    Update 2ndTest.java

--- 2ndTest.java
+++ 2ndTest.java
@@ -3,3 +3,4 @@ Sampath
 Kannaboina
 Updating SubBranch with Master branch Changes or updates
 Updating SubBranch with Master branch Changes or updates
+Updating SubBranch with Master branch Changes or updates..... with "git rebase master" . in git checkout suBranchName
