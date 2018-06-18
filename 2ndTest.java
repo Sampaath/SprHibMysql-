e61dd008892f655ea9b44982c2592a78a493f5c8
Sampaath <31530528+Sampaath@users.noreply.github.com>
2018-06-18 15:38:19 -0500

    Update 2ndTest.java

--- 2ndTest.java
+++ 2ndTest.java
@@ -1,10 +1,1 @@
-dsmaDA
 
-dsmaDA
-Sampath
-Kannaboina
-Updating SubBranch with Master branch Changes or updates
-Updating SubBranch with Master branch Changes or updates
-Updating SubBranch with Master branch Changes or updates..... with "git rebase master" . in git checkout suBranchName
-dsvn,dac
-djvn.dacad
