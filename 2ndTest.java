756d92b4098b97d4410fce66e7b2e3f52891d905
Sampaath <31530528+Sampaath@users.noreply.github.com>
2018-06-19 10:12:39 -0500

    Update 2ndTest.java

--- 2ndTest.java
+++ 2ndTest.java
@@ -1,3 +1,3 @@
 #End point URL for PSG service
-
+#Sampath
 psgWebServiceEndPointUrl=http://nlg00667.tst.kohls.com:30010/Processes/Business%20Processes/Starter%20Processes/Services/intfretrievePSGRMS13Details-service.serviceagent/PortTypeEndpoint
