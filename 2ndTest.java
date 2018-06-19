aed38b84533f796f471f16604ca8d124b7d671ca
SAMPATH <sampaaaath@gmail.com>
2018-06-19 10:19:19 -0500

    Test6

--- 2ndTest.java
+++ 2ndTest.java
@@ -3,3 +3,4 @@
 psgWebServiceEndPointUrl=http://nlg00667.tst.kohls.com:30010/Processes/Business%20Processes/Starter%20Processes/Services/intfretrievePSGRMS13Details-service.serviceagent/PortTypeEndpoint
 
 Test5
+TEST6
