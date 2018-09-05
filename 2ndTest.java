784324dc3f94217f023b49179160c138a5807d98
Sampaath <31530528+Sampaath@users.noreply.github.com>
2018-09-05 10:46:16 -0500

    Update 2ndTest.java

--- 2ndTest.java
+++ 2ndTest.java
@@ -1,7 +1,8 @@
-Hey Master branch
+Hey we are master branch
 #### OLD URLS##
 <<<<<<< HEAD
 ## CHECKING UPDATES###
 mvZ:V
 =======
 
+these are the changes that all the other team mates did, and we need to add these to our code
